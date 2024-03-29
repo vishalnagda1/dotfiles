012ef2fad52d77344bb07e47e42f861fa824f96c
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2024-03-29 17:08:19 +0530

    Disable pyright lsp config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -540,7 +540,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {
+        --[[ pyright = {
           settings = {
             python = {
               analysis = {
@@ -551,7 +551,7 @@ require('lazy').setup({
               },
             },
           },
-        },
+        }, ]]
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
